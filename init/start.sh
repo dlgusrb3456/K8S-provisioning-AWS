1a244e088f70fc4e74c4628f616673ad5f4979ee
dlgusrb3456 <77333310+dlgusrb3456@users.noreply.github.com>
2022-12-17 21:52:38 +0900

    Update start.sh

--- init/start.sh
+++ init/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cd /root/terraformlab
+cd [pwd terraformlab directory]
 terraform init
 terraform plan -auto-approve
 terraform apply -auto-approve
+
 sleep 5
-master_ip=$(cat /root/masterip)
+master_ip=$(cat ./masterip)
 
 echo $master_ip
 sleep 5
@@ -14,15 +15,15 @@ $(ssh-keyscan $master_ip >> /root/.ssh/known_hosts) > /dev/null
 $(ssh-keyscan $master_ip >> /root/.ssh/known_hosts) > /dev/null
 $(ssh-keyscan $master_ip >> /root/.ssh/known_hosts) > /dev/null
 
-cd
+cd [pwd init directory]
 ansible-playbook main-playbook.yaml
 echo "install calico"
 ansible master -m shell -a "sudo kubectl apply -f calico.yaml"
 
 
-
+cd [pwd workerlab directory]
 cd /root/workerlab
-echo "sudo $(tail -n 2 /root/init_result.txt)" >> worker_install_k8s.sh
+echo "sudo $(tail -n 2 ../init/init_result.txt)" >> worker_install_k8s.sh
 echo "stress --cpu 100" >> worker_install_k8s.sh
 terraform init
 terraform plan -auto-approve
